2efff426d86e7af24a7d1f0fd2d067ddde337f32
Akhilesh18161 <akhilesh.18161@gmail.com>
2025-12-25 11:39:03 +0545

    Made more usable!

--- bank_program.py
+++ bank_program.py
@@ -1,11 +1,12 @@
 # **************** WELCOME ************************
 
-is_running = True
+
 print("Welcome to Bank program! \n******************************")
 print()
 
 
 def bank_program():
+    is_running = True
     bale = 0.0
     while is_running:
         print()
@@ -13,21 +14,32 @@ def bank_program():
 
         usr_inpt = int(input("Enter a choice (1-4): "))
 
+        def inpt_check():
+            nonlocal is_running
+            inpt = int(input("Back (1)\nQuit (2)\n => "))
+            if inpt == 1:
+                pass
+            elif inpt == 2:
+                print("Have a Nice Day!\nExiting......")
+                is_running = False
+
         def balance():
             print(f"\n\nYour Balance is ${bale:.2f}\n")
-            bank_program()
+            inpt_check()
 
         def deposite():
             nonlocal bale
             amnt = float(input("Enter the amount you want to deposite: $"))
             bale += amnt
-            print(f"${amnt} Was sucessfully added")
+            print(f"${amnt} Was sucessfully added\n")
+            inpt_check()
 
         def withdraw():
             nonlocal bale
             amnt = float(input("Enter the amount you want to Withdraw: $"))
             bale -= amnt
             print(f"${amnt} was sucessfully withdrawn")
+            inpt_check()
 
         match usr_inpt:
             case 1:
@@ -37,7 +49,8 @@ def bank_program():
             case 3:
                 withdraw()
             case 4:
-                break
+                print("Have a Nice Day!\nExiting......")
+                is_running = False
 
 
 bank_program()
