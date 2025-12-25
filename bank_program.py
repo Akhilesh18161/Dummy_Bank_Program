3c1e50135dad32a869079b7c6c77b9537eede920
Akhilesh18161 <akhilesh.18161@gmail.com>
2025-12-25 10:29:32 +0545

    Initial Commit

---
+++ bank_program.py
@@ -0,0 +1,41 @@
+is_running = True
+print("Welcome to Bank program! \n******************************")
+print()
+
+
+def bank_program():
+    bale = 0.0
+    while is_running:
+        print()
+        print("1. Balance Check\n2. Deposite Amount\n3. Withdraw Amount\n4. Exit")
+
+        usr_inpt = int(input("Enter a choice (1-4): "))
+
+        def balance():
+            print(f"\n\nYour Balance is ${bale:.2f}\n")
+            bank_program()
+
+        def deposite():
+            nonlocal bale
+            amnt = float(input("Enter the amount you want to deposite: $"))
+            bale += amnt
+            print(f"${amnt} Was sucessfully added")
+
+        def withdraw():
+            nonlocal bale
+            amnt = float(input("Enter the amount you want to Withdraw: $"))
+            bale -= amnt
+            print(f"${amnt} was sucessfully withdrawn")
+
+        match usr_inpt:
+            case 1:
+                balance()
+            case 2:
+                deposite()
+            case 3:
+                withdraw()
+            case 4:
+                break
+
+
+bank_program()
